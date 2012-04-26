1c269bbdb33c4c8701f96e2f15496664c40f0131
kawatoto <kawatoto@kawatoto-Netbook>
2012-04-25 23:32:56 -0500

    25-04-2012T23:22:00

--- Documents/NetBeansProjects/ISFE-20110020/src/java/subsistemaAutomatico/EnvioMail.java
+++ Documents/NetBeansProjects/ISFE-20110020/src/java/subsistemaAutomatico/EnvioMail.java
@@ -3,6 +3,7 @@ package subsistemaAutomatico;
 import java.io.File;
 import java.util.Properties;
 import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
@@ -17,81 +18,89 @@ import javax.mail.internet.MimeMultipart;
 public class EnvioMail
 {
     final String correo = "isfe20110020@gmail.com";
-    final String contraseña = "isfe20110020";
+    final String contrasena = "isfe20110020";
     final String servidorSMTP = "smtp.gmail.com";
-    final String puertoEnvio = "465";
+    final String puertoEnvio = "587";
     String mailReceptor = null;
     String asunto = null;
     String cuerpo = null;
-
-        /**
-         *
-         * @param mailReceptor parametro encargado de ser el mail del receptor
-         * en este caso del usuario de ISFE
-         * @param asunto parametro encrgado de mostrar al receptor del mail el
-         * asunto del mensaje
-         * @param cuerpo es el cuerpo del mensaje al destinatario del mail
-         * @param xml es el archivo adjunto, concretamente la factura electronica
-         * que se enviara al usuario
-         * @throws MessagingException excepcion que maneja algun problema al
-         * realizar el envio del correo
-         */
-        public EnvioMail(String mailReceptor, String asunto, String cuerpo,File xml) throws MessagingException {
+    /**
+     * Método encargado de realizar el envió del correo electrónico al Usuario
+     * con la factura eletrónica generada en formato xml
+     * @param mailReceptor parametro encargado de ser el mail del receptor
+     * en este caso del usuario de ISFE
+     * @param asunto parametro encrgado de mostrar al receptor del mail el
+     * asunto del mensaje
+     * @param cuerpo es el cuerpo del mensaje al destinatario del mail
+     * @param xml es el archivo adjunto, concretamente la factura electronica
+     * que se enviara al usuario
+     * @throws MessagingException excepcion que maneja algun problema al
+     * realizar el envio del correo
+     */
+    public EnvioMail(String mailReceptor, String asunto, String cuerpo, File archivo, String nameArchivo)
+            throws MessagingException {
         this.mailReceptor = mailReceptor;
         this.asunto = asunto;
         this.cuerpo = cuerpo;
 
-        Properties props = new Properties();
-        props.put("mail.smtp.user", correo);
-        props.put("mail.smtp.host", servidorSMTP);
-        props.put("mail.smtp.port", puertoEnvio);
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.socketFactory.port", puertoEnvio);
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.socketFactory.fallback", "false");
+        try
+        {
+            Properties props = new Properties();
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.setProperty("mail.smtp.starttls.enable", "true");
+            props.setProperty("mail.smtp.port", puertoEnvio);
+            props.setProperty("mail.smtp.user", correo);
+            props.setProperty("mail.smtp.auth", "true");
+            Session session = Session.getDefaultInstance(props, null);
+            session.setDebug(true);
 
-        SecurityManager security = System.getSecurityManager();
+            // Se compone la parte del texto
+            MimeBodyPart texto = new MimeBodyPart();
+            texto.setText(this.cuerpo);
 
-        // Se compone la parte del texto
-        BodyPart texto = new MimeBodyPart();
-        texto.setText(cuerpo);
+            // Se compone el adjunto con la imagen
+            MimeBodyPart adjunto = new MimeBodyPart();
+            adjunto.setDataHandler(new DataHandler(new FileDataSource(archivo)));
+            adjunto.setFileName(nameArchivo);
 
+            // Una MultiParte para agrupar texto e imagen.
+            MimeMultipart multiParte = new MimeMultipart();
+            multiParte.addBodyPart(texto);
+            multiParte.addBodyPart(adjunto);
 
-        // Se compone el adjunto con la imagen
-        BodyPart adjunto = new MimeBodyPart();
-        Object archivoAdjunto = xml;
-        adjunto.setDataHandler(new DataHandler(archivoAdjunto,"xml"));
-        //adjunto.setDataHandler(new DataHandler(new FileDataSource("")));
-        adjunto.setFileName("Factura Electronica");
+            // Se compone el correo, dando to, from, subject y el
+            // contenido.
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(correo));
+            message.addRecipient(
+                Message.RecipientType.TO,
+                new InternetAddress(this.mailReceptor));
+            message.setSubject(this.asunto);
+            message.setContent(multiParte);
 
-        // Una MultiParte para agrupar texto e imagen.
-        MimeMultipart multiParte = new MimeMultipart();
-        multiParte.addBodyPart(texto);
-        multiParte.addBodyPart(adjunto);
-
-        try {
-            Authenticator auth = new EnvioMail.autentificadorSMTP();
-            Session session = Session.getInstance(props, auth);
-            // session.setDebug(true);
-
-            MimeMessage msg = new MimeMessage(session);
-            msg.setSubject(asunto);
-            msg.setContent(multiParte);
-            msg.setFrom(new InternetAddress(correo));
-            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(
-                    mailReceptor));
-            Transport.send(msg);
-        } catch (Exception mex) {
+            // Se envia el correo.
+            Transport t = session.getTransport("smtp");
+            t.connect(correo, contrasena);
+            t.sendMessage(message, message.getAllRecipients());
+            t.close();
         }
-
+        catch (Exception e)
+        {
+        }
     }
-
+    /**
+     * Clase encargada de la autentificación del SMTP para el envió de correos
+     * en ISFE
+     */
     private class autentificadorSMTP extends javax.mail.Authenticator {
+        /**
+         * Método encargado de la autentificación para el envió y recepción para
+         * el uso de SMTP para envió de correos electrónicos
+         * @return
+         */
         @Override
         public PasswordAuthentication getPasswordAuthentication() {
-            return new PasswordAuthentication(correo, contraseña);
+            return new PasswordAuthentication(correo, contrasena);
         }
     }
 }
---
+++ Documents/NetBeansProjects/ISFE-20110020/src/java/subsistemaAutomatico/ReportesMensuales.java
@@ -0,0 +1,111 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package subsistemaAutomatico;
+
+import dao.Sql;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Calendar;
+import javax.mail.MessagingException;
+
+/**
+ *
+ * @author kawatoto
+ */
+public class ReportesMensuales
+{
+    /**
+     * Estos atributos son para conformar el nombre del reporte mensual
+     */
+    private char numeroEsquema;
+    private String RFCEmisor;
+    private String mesReportado;
+    private String anioReportado;
+
+    /**
+     * Estos atributos son de los datos contenidos en el reporte mensual
+     */
+    private String RFCCliente[];
+    private String serie[];
+    private String anioAprobacion[];
+    private String numeroAprobacion[];
+
+    private String fechaYHoraEmision[];
+    private String montoOperacion[];
+    private Double montoIVA[];
+    private String estadoComprobante[];//cancelado o vigente
+    private String elementoReporte[];
+
+    public boolean generarReportes(String RFCEmisor, String MesReportado, String anioReportado)
+            throws InstantiationException, SQLException, IllegalAccessException, IOException, MessagingException
+    {
+        this.numeroEsquema=1;
+        this.RFCEmisor=RFCEmisor;
+        this.mesReportado=MesReportado;
+        this.anioReportado=anioReportado;
+
+        String nombreArchivo=this.numeroEsquema+this.RFCEmisor+this.mesReportado+this.anioReportado+".txt";
+
+        Sql sql = new Sql();
+        Calendar fechaActual = Calendar.getInstance();
+        String consulta="SELECT usuario.idUsuario, rfc, serie, yearAprobacion, "
+                + "noAprobacion, fechaElaboracion,importeTotal,estadoComprobante FROM usuario "
+                + "JOIN factura WHERE fechaElaboracion<'"+fechaActual.get(Calendar.YEAR)+"-"+(fechaActual.get(Calendar.MONTH)+2)+"-01' "
+                + "and fechaElaboracion>'"+fechaActual.get(Calendar.YEAR)+"-"+fechaActual.get(Calendar.MONTH)+"-01'";
+
+        System.out.println(consulta);
+        ResultSet rs = sql.consulta(consulta);
+
+        rs.last();
+        int i=rs.getRow();
+        RFCCliente=new String[i];
+        System.out.println(RFCCliente.length);
+        serie=new String[i];
+        anioAprobacion=new String[i];
+        numeroAprobacion=new String[i];
+        fechaYHoraEmision=new String[i];
+        montoOperacion=new String[i];
+        montoIVA=new Double[i];
+        estadoComprobante=new String[i];
+        elementoReporte=new String[i];
+        rs.first();
+
+        while(rs.next())
+        {
+            for(int j=0;j<i;j++)
+            {
+                RFCCliente[j]=rs.getString("rfc");
+                System.out.println(RFCCliente[j]);
+                serie[j]=rs.getString("serie");
+                anioAprobacion[j]=rs.getString("yearAprobacion");
+                numeroAprobacion[j]=rs.getString("noAprobacion");
+                fechaYHoraEmision[j]=rs.getString("fechaElaboracion");
+                montoOperacion[j]=rs.getString("importeTotal");
+                montoIVA[j]=rs.getDouble("importeTotal")*(0.16);
+                estadoComprobante[j]=rs.getString("estadoComprobante");
+                elementoReporte[j]="||"+RFCCliente[j]+"|"+serie[j]+""
+                        + "|"+anioAprobacion[j]+"|"+numeroAprobacion[j]+""
+                        + "|"+fechaYHoraEmision[j]+"|"+montoOperacion[j]+""
+                        + "|"+montoIVA[j]+"|"+estadoComprobante[j]+"||";
+            }
+        }
+
+        File reporteMensual = new File(nombreArchivo);
+        BufferedWriter bw = new BufferedWriter(new FileWriter(nombreArchivo));
+        for(int j=0;j<i;j++)
+        {
+            System.out.println(elementoReporte[j]);
+            bw.write(elementoReporte[j]+"\n");
+        }
+        bw.close();
+        EnvioMail mail = new EnvioMail("kawatoto.j33@gmail.com","Reporte Mensual","Archivo del Reporte Mensual del SAT", reporteMensual ,nombreArchivo);
+        reporteMensual.delete();
+        return true;
+    }
+}
